a4d2a0378eccf23e34e9512de1b89ff811904786
arash <arash@ARASHITORA>
2021-11-19 01:28:19 -0400

    Add project files.

---
+++ Do While 1er Programa/Do While 1er Programa.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numero;
+    do
+    {
+        cout << "Ingrese un numero ";
+        cin >> numero;
+    } while (numero <= 10);
+    return 0;
+}
